e3fe9c999cbbb96b6922c1c4855aa31e07da84a9
PhamTanHai3012 <145772246+PhamTanHai3012@users.noreply.github.com>
2023-10-27 09:00:15 +0700

    PTUDW
    27/10/2023

--- PTUDW/MyClass/Model/Categories.cs
+++ PTUDW/MyClass/Model/Categories.cs
@@ -14,8 +14,8 @@ namespace MyClass.Model
         [Key]
         public int Id { get; set; }
 
-        [Required]
         [Display(Name="Tên loại SP")]
+        [Required(ErrorMessage = "Tên loại sản phẩm không để rỗng")]
         public string Name { get; set; }
 
         [Display(Name = "Tên rút gọn")]
@@ -27,27 +27,32 @@ namespace MyClass.Model
         [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Mô tả không để trống")]
         [Display(Name = "Mô tả")]
         public string MetaDesc { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Từ khóa không để rỗng")]
         [Display(Name = "Từ khóa")]
         public string MetaKey { get; set; }
 
         [Display(Name = "Tạo bởi")]
+        [Required(ErrorMessage = "Người tạo không để rỗng")]
         public int CreateBy { get; set; }
 
         [Display(Name = "Ngày tạo")]
+        [Required(ErrorMessage = "Ngày tạo không để rỗng")]
         public DateTime CreateAt { get; set; }
 
         [Display(Name = "Cập nhật bởi")]
+        [Required(ErrorMessage = "Người cập nhật không để rỗng")]
         public int UpdateBy { get; set; }
 
         [Display(Name = "Ngày cập nhật")]
+        [Required(ErrorMessage = "Ngày cập nhật không để rỗng")]
         public DateTime UpdateAt { get; set; }
 
         [Display(Name = "Trạng thái")]
+        [Required(ErrorMessage = "Trạng thái không để rỗng")]
         public int Status { get; set; }
 
     }
--- PTUDW/PTUDW/Areas/Admin/Controllers/CategoryController.cs
+++ PTUDW/PTUDW/Areas/Admin/Controllers/CategoryController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using MyClass.Model;
 using MyClass.DAO;
+using PTUDW.Library;
 
 namespace PTUDW.Areas.Admin.Controllers
 {
@@ -27,12 +28,16 @@ namespace PTUDW.Areas.Admin.Controllers
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Không tồn tại loại sản phẩm");
+                return RedirectToAction("Index");
             }
             Categories categories = categoriesDAO.getRow(id);
             if (categories == null)
             {
-                return HttpNotFound();
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Không tồn tại loại sản phẩm");
+                return RedirectToAction("Index");
             }
             return View(categories);
         }
@@ -41,6 +46,8 @@ namespace PTUDW.Areas.Admin.Controllers
         // GET: Admin/Category/Create
         public ActionResult Create()
         {
+            ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+            ViewBag.ListOrder = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
             return View();
         }
 
@@ -50,11 +57,37 @@ namespace PTUDW.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                //Xu ly tu dong: CreateAt
+                categories.CreateAt = DateTime.Now;
+                //Xu ly tu dong: UpdateAt
+                categories.UpdateAt = DateTime.Now;
+                //Xu ly tu dong: ParentId
+                if(categories.ParentID == null)
+                {
+                    categories.ParentID = 0;
+                }
+                //Xu ly tu dong: Order
+                if(categories.Order == null)
+                {
+                    categories.Order = 1;
+                }
+                else
+                {
+                    categories.Order += 1;  
+                }
+                //Xu ly tu dong: Slug
+                categories.Slug = XString.Str_Slug(categories.Name);
+
+                //Chen them dong cho DB
                 categoriesDAO.Insert(categories);
+                //Thong bao cap nhat trang thai thanh cong
+                TempData["message"] = TempData["message"] = new XMessage("success", "Tạo mới loại sản phẩm thành công");
+                //Tro ve trang Index
                 return RedirectToAction("Index");
             }
-
-            return View();
+            ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+            ViewBag.ListOrder = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
+            return View(categories);
         }
 
         //EDIT
@@ -63,13 +96,19 @@ namespace PTUDW.Areas.Admin.Controllers
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Không tìm thấy mẫu tin");
+                return RedirectToAction("Index");
             }
             Categories categories = categoriesDAO.getRow(id);
             if (categories == null)
             {
-                return HttpNotFound();
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Không tìm thấy mẫu tin");
+                return RedirectToAction("Index");
             }
+            ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+            ViewBag.ListOrder = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
             return View(categories);
         }
 
@@ -79,9 +118,34 @@ namespace PTUDW.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                //Xu ly tu dong: Slug
+                categories.Slug = XString.Str_Slug(categories.Name);
+                //Xu ly tu dong: ParentID
+                if(categories.ParentID == null)
+                {
+                    categories.ParentID = 0;
+                }
+                //Xủ ly tu dong: Order
+                if(categories.Order == null)
+                {
+                    categories.Order = 1;
+                }
+                else
+                {
+                    categories.Order += 1;
+                }
+                //Xu ly tu dong: UpdateAt
+                categories.UpdateAt = DateTime.Now;
+                
+                //Cap nhat mau tin
                 categoriesDAO.Update(categories);
+
+                //Thong bao cap nhat trang thai thanh cong
+                TempData["message"] = TempData["message"] = new XMessage("success", "Cập nhật mẫu tin thành công");
                 return RedirectToAction("Index");
             }
+            ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+            ViewBag.ListOrder = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
             return View(categories);
         }
 
@@ -91,12 +155,16 @@ namespace PTUDW.Areas.Admin.Controllers
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Xóa mẫu tin thất bại");
+                return RedirectToAction("Index");
             }
             Categories categories = categoriesDAO.getRow(id);
             if (categories == null)
             {
-                return HttpNotFound();
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Xóa mẫu tin thất bại");
+                return RedirectToAction("Index");
             }
             return View(categories);
         }
@@ -109,7 +177,124 @@ namespace PTUDW.Areas.Admin.Controllers
             Categories categories = categoriesDAO.getRow(id);
             categoriesDAO.Delete(categories);
 
-            return RedirectToAction("Index");
+            //Thong bao thanh cong
+            TempData["message"] = TempData["message"] = new XMessage("success", "Xóa mẫu tin thành công");
+            return RedirectToAction("Trash");
+        }
+
+
+        //STATUS
+        // GET: Admin/Category/Status/5
+        public ActionResult Status(int? id)
+        {
+            if (id == null)
+            {
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+                return RedirectToAction("Index");
+            }
+            //Truy van Id
+            Categories categories = categoriesDAO.getRow(id);
+            if (categoriesDAO == null) 
+            {
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //Chuyen doi trang thai cua Status 1 <- -> 2
+                categories.Status = (categories.Status == 1) ? 2 : 1;
+
+                //Cap nhat gia tri UpdateAt
+                categories.UpdateAt = DateTime.Now;
+
+                //Cap nhat lai DB
+                categoriesDAO.Update(categories);
+
+                //Thong bao cap nhat trang thai thanh cong
+                TempData["message"] = TempData["message"] = new XMessage("success", "Cập nhật trạng thái thành công");
+
+                return RedirectToAction("Index");
+            }
+        }
+        //Deltrash
+        // GET: Admin/Category/Deltrash/5
+        public ActionResult DelTrash(int? id)
+        {
+            if (id == null)
+            {
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Không tìm thấy mẫu tin");
+                return RedirectToAction("Index");
+            }
+            //Truy van Id
+            Categories categories = categoriesDAO.getRow(id);
+            if (categoriesDAO == null)
+            {
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Không tìm thấy mẫu tin");
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //Chuyen doi trang thai cua Status 1,2 -> 0 : không hiển thị ở Index
+                categories.Status = 0;
+
+                //Cap nhat gia tri UpdateAt
+                categories.UpdateAt = DateTime.Now;
+
+                //Cap nhat lai DB
+                categoriesDAO.Update(categories);
+
+                //Thong bao cap nhat trang thai thanh cong
+                TempData["message"] = TempData["message"] = new XMessage("success", "Xóa mẫu tin thành công");
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        //Trash
+        // GET: Admin/Trash
+        public ActionResult Trash()
+        {
+            return View(categoriesDAO.getList("Trash"));
+        }
+
+        //RECOVER       
+        // GET: Admin/Category/Recover/5
+        public ActionResult Recover(int? id)
+        {
+            if (id == null)
+            {
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Phục hồi mẫu tin thất bại");
+                return RedirectToAction("Index");
+            }
+            //Truy van Id
+            Categories categories = categoriesDAO.getRow(id);
+            if (categoriesDAO == null)
+            {
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Phục hồi mẫu tin thất bại");
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //Chuyen doi trang thai cua Status 0-->2 : Khong xuat ban
+                categories.Status = 2;
+
+                //Cap nhat gia tri UpdateAt
+                categories.UpdateAt = DateTime.Now;
+
+                //Cap nhat lai DB
+                categoriesDAO.Update(categories);
+
+                //Thong bao mau tin trang thai thanh cong
+                TempData["message"] = TempData["message"] = new XMessage("success", "Phục hồi mẫu tin thành công");
+
+                return RedirectToAction("Index");
+            }
         }
     }
 }
